70696b5c248b93f501044d54a2b77882c0554f94
GusVieweg <awvieweg@gmail.com>
2019-12-18 10:24:59 -0500

    first commit, works

---
+++ spotify_creds.py
@@ -0,0 +1,6 @@
+def get_credentials():
+    return {
+        "username": "winchopband@gmail.com",
+        "password": "WilsonandGus!",
+        "feature_song_url": "spotify:track:1TUh9menluz9TR9jS6UVl5"
+    }
---
+++ update_spotify.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import spotify_creds
+import time
+
+# Get credentials
+auth = spotify_creds.get_credentials()
+
+# Fire up a browser
+browser = webdriver.Firefox()
+browser.get('https://accounts.spotify.com/en/login?continue=https:%2F%2Fartists.spotify.com%2F')
+
+# Log in
+browser.find_element_by_id('login-username').send_keys(auth['username'])
+browser.find_element_by_id('login-password').send_keys(auth['password'])
+browser.find_element_by_xpath("//label[starts-with(@class, 'ng-binding')]").click() # Uncheck "Remember me for next time"
+browser.find_element_by_id('login-button').click()
+
+# Give the server time to authenticate
+time.sleep(5)
+
+# Force a redirect and go to Profile
+browser.get('https://artists.spotify.com/c/')
+time.sleep(3)
+browser.get(browser.current_url.replace('home', 'profile'))
+time.sleep(3)
+
+# Update feature song
+browser.find_element_by_xpath("//*[contains(text(), 'feature music')]").click()
+browser.find_element_by_xpath("//input[starts-with(@class, 'EntityPicker')]").send_keys(auth['feature_song_url'])
+time.sleep(5) # Give the server time to render song selection
+browser.find_element_by_xpath("//div[starts-with(@class, 'ImageWithText')]").click()
+browser.find_element_by_xpath("//label[starts-with(@class, 'Checkbox')]").click()
+browser.find_element_by_xpath("//input[starts-with(@class, 'Comment')]").send_keys("Thank you!")
+browser.find_element_by_xpath("//span[contains(text(), 'Save')]").click()
+
+# Close browser
+browser.close()
