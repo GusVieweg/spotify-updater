12520ff368592c4355775bfa6c762c3a5db444be
GusVieweg <awvieweg@gmail.com>
2020-01-02 12:17:43 -0500

    added logging and changed time

--- SpotifyTools.py
+++ SpotifyTools.py
@@ -14,6 +14,7 @@ class SpotifyUpdater:
 
     def update_feature_song(self):
         # Fire up a browser
+        print("Firing up a Chrome browser...")
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ['CHROME_PATH']
         chrome_options.add_argument("--headless")
@@ -21,12 +22,15 @@ class SpotifyUpdater:
         chrome_options.add_argument("--no-sandbox")
         browser = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], options=chrome_options)
         browser.get('https://accounts.spotify.com/en/login?continue=https:%2F%2Fartists.spotify.com%2F')
+        print("Complete!")
 
         # Log in
+        print("Accessing profile...")
         browser.find_element_by_id('login-username').send_keys(self.SPOTIFY_USERNAME)
         browser.find_element_by_id('login-password').send_keys(self.SPOTIFY_PASSWORD)
         browser.find_element_by_xpath("//label[starts-with(@class, 'ng-binding')]").click() # Uncheck "Remember me for next time"
         browser.find_element_by_id('login-button').click()
+        print("Complete!")
 
         # Give the server time to authenticate
         time.sleep(5)
@@ -37,6 +41,7 @@ class SpotifyUpdater:
         browser.get(browser.current_url.replace('home', 'profile'))
         time.sleep(3)
 
+        print("Selecting feature song...")
         # Update feature song
         browser.find_element_by_xpath("//*[contains(text(), 'feature music')]").click()
         browser.find_element_by_xpath("//input[starts-with(@class, 'EntityPicker')]").send_keys(self.SPOTIFY_SONG_URL)
@@ -45,6 +50,7 @@ class SpotifyUpdater:
         browser.find_element_by_xpath("//label[starts-with(@class, 'Checkbox')]").click()
         browser.find_element_by_xpath("//input[starts-with(@class, 'Comment')]").send_keys("Thank you!")
         browser.find_element_by_xpath("//span[contains(text(), 'Save')]").click()
+        print("Complete!")
 
         # Close browser
         browser.close()
--- clock.py
+++ clock.py
@@ -4,10 +4,12 @@ from SpotifyTools import SpotifyUpdater
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', days=14, start_date='2020-01-02 12:12:00', timezone="US/Eastern")
+@sched.scheduled_job('interval', days=14, start_date='2020-01-02 12:20:00', timezone="US/Eastern")
 def scheduled_job():
     su = SpotifyUpdater()
+    print("Initialized Spotify Updater.")
     su.update_feature_song()
+    print("Feature song updated.")
 
 
 sched.start()
