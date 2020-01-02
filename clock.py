f5342a3dd979338acdca5c569209ee2d7abba537
GusVieweg <awvieweg@gmail.com>
2020-01-02 09:59:46 -0500

    updated time, added firefox buildpack

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from SpotifyTools import SpotifyUpdater
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', days=14, start_date='2019-12-19 00:01:00')
+@sched.scheduled_job('interval', days=14, start_date='2020-01-02 10:05:00')
 def scheduled_job():
     su = SpotifyUpdater()
     su.update_feature_song()
