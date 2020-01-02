f176ada6d967f3c4494e1a4e0b1ec5586e545f34
GusVieweg <awvieweg@gmail.com>
2020-01-02 10:58:15 -0500

    updated time and drivers

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from SpotifyTools import SpotifyUpdater
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', days=14, start_date='2020-01-02 10:40:00', timezone="US/Eastern")
+@sched.scheduled_job('interval', days=14, start_date='2020-01-02 11:00:00', timezone="US/Eastern")
 def scheduled_job():
     su = SpotifyUpdater()
     su.update_feature_song()
